80fa0054dfccfb997eb48137b9fdabdcbbcd887c
Yassine Naanani <27584700+K11E3R@users.noreply.github.com>
2020-11-11 12:00:00 +0100

    Python IP Tracker

---
+++ README.md
@@ -0,0 +1,94 @@
+# Python IP Tracker
+
+<div align="center">
+
+  ![GitHub repo size](https://img.shields.io/github/repo-size/K11E3R/Python-IP-Tracker) ![GitHub contributors](https://img.shields.io/github/contributors/K11E3R/Python-IP-Tracker)
+</div>
+
+<div align="center">
+  <img src="YouSeeMe.png" alt="NowYouSeeMe">
+  <p>
+  <br>Python IP Tracker is a command-line tool that retrieves detailed geographical and network information for a given IP address from multiple API sources.</p>
+</div>
+
+## Features
+
+- Retrieve IP information such as city, region, country, continent, timezone, currency, and more.
+- Supports multiple API sources for redundancy and reliability.
+- Color-coded output for better readability using `colorama`.
+- Cross-platform compatibility (Windows, macOS, Linux).
+
+## Installation
+
+### Prerequisites
+
+- Python 3.7 or higher installed.
+- `pip` package manager.
+
+### Installation Steps
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/K11E3R/Python-IP-Tracker.git
+   ```
+
+2. Navigate to the project directory:
+
+   ```bash
+   cd Python-IP-Tracker
+   ```
+
+3. Install dependencies:
+
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+## Usage
+
+1. Run the script:
+
+   ```bash
+   python ip_tracker.py
+   ```
+
+2. Enter the IP address you want to track when prompted.
+
+3. View detailed information about the IP address provided.
+
+4. Type `exit` to quit the program.
+
+## API Sources
+
+- [ipdata.co](https://api.ipdata.co/)
+- [ipinfo.io](https://ipinfo.io/)
+- [ipapi.co](https://ipapi.co/)
+- [freegeoip.app](https://freegeoip.app/)
+- [ipgeolocation.io](https://ipgeolocation.io/)
+- [extreme-ip-lookup.com](https://extreme-ip-lookup.com/)
+- [ipwhois.app](https://ipwhois.app/)
+
+## Contributing
+
+Contributions are welcome! Please follow these steps:
+
+1. Fork the repository.
+2. Create a new branch (`git checkout -b feature/your-feature`).
+3. Make your changes.
+4. Commit your changes (`git commit -am 'Add some feature'`).
+5. Push to the branch (`git push origin feature/your-feature`).
+6. Create a new Pull Request.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+## Acknowledgments
+
+- [Colorama](https://pypi.org/project/colorama/) for terminal colors.
+- API providers for their free IP geolocation services.
+
+## Contact
+
+Yassine Naanani - [LinkedIn](https://www.linkedin.com/in/yassine-naanani-5332a7a0/)
---
+++ YouSeeMe.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python3
+
+import sys
+import subprocess
+import importlib.util
+import asyncio
+import aiohttp
+import json
+import colorama
+import os
+
+# Function to check and install required libraries if not present
+def check_install_libraries(libraries):
+    missing_libraries = []
+    for lib in libraries:
+        spec = importlib.util.find_spec(lib)
+        if spec is None:
+            missing_libraries.append(lib)
+            print(f"{colorama.Fore.YELLOW}Library '{lib}' is not installed.")
+
+    if missing_libraries:
+        print(colorama.Fore.YELLOW + "Attempting to install missing libraries...")
+        try:
+            subprocess.check_call([sys.executable, '-m', 'pip', 'install'] + missing_libraries)
+        except subprocess.CalledProcessError:
+            print(colorama.Fore.RED + "Failed to install missing libraries. Please install manually.")
+            return False
+    return True
+
+# Check and install required libraries
+required_libraries = ['aiohttp', 'colorama']
+if not check_install_libraries(required_libraries):
+    sys.exit(1)
+
+# List of API URLs to test and use
+API_URLS = [
+    "https://api.ipdata.co/",
+    "https://ipinfo.io/",
+    "https://ipapi.co/",
+    "https://freegeoip.app/json/",
+    "https://api.ipgeolocation.io/ipgeo",
+    "https://extreme-ip-lookup.com/json/",
+    "https://ipwhois.app/json/",
+]
+
+def clear_screen():
+    """Function to clear the screen based on OS type."""
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+async def check_api_access(session, url):
+    """Async function to check if the API URL is accessible."""
+    try:
+        async with session.get(url, timeout=10) as response:
+            return response.status == 200
+    except aiohttp.ClientError:
+        return False
+    except asyncio.TimeoutError:
+        return False
+
+MAX_RETRY_ATTEMPTS = 3
+
+async def fetch_ip_data(session, url, ip):
+    """Async function to fetch IP data from API."""
+    retry_count = 0
+    while retry_count < MAX_RETRY_ATTEMPTS:
+        try:
+            async with session.get(f"{url}{ip}", timeout=10) as response:
+                if response.status == 200:
+                    return await response.json()
+                else:
+                    print(colorama.Fore.YELLOW + f"Failed to fetch data from {url}. Retrying...")
+        except aiohttp.ClientError:
+            print(colorama.Fore.YELLOW + f"Failed to connect to {url}. Retrying...")
+        except asyncio.TimeoutError:
+            print(colorama.Fore.YELLOW + f"Timeout occurred while connecting to {url}. Retrying...")
+        
+        retry_count += 1
+    
+    return None
+
+def print_ip_data(values):
+    """Function to print IP data."""
+    if values:
+        print("------------------------------------")
+        print(f" {colorama.Fore.GREEN}IP           :  {values.get('ip')}")
+        print(f" {colorama.Fore.GREEN}City         :  {values.get('city')}")
+        print(f" {colorama.Fore.GREEN}Region       :  {values.get('region')}")
+        print(f" {colorama.Fore.GREEN}Country      :  {values.get('country_name')}")
+        print(f" {colorama.Fore.GREEN}Continent    :  {values.get('continent_name')}")
+        print(f" {colorama.Fore.GREEN}Time Zone    :  {values.get('time_zone')}")
+        print(f" {colorama.Fore.GREEN}Currency     :  {values.get('currency')}")
+        print(f" {colorama.Fore.GREEN}Calling Code :  +{values.get('calling_code')}")
+        print(f" {colorama.Fore.GREEN}Organisation :  {values.get('organisation')}")
+        print(f" {colorama.Fore.GREEN}ASN          :  {values.get('asn')}")
+        print("------------------------------------")
+    else:
+        print(colorama.Fore.RED + "No data found for the provided IP.")
+
+async def test_api_urls():
+    """Async function to test API URLs for accessibility."""
+    async with aiohttp.ClientSession() as session:
+        accessible_urls = []
+        for url in API_URLS:
+            access_allowed = await check_api_access(session, url)
+            if access_allowed:
+                accessible_urls.append(url)
+                print(colorama.Fore.GREEN + f"API URL {url} is accessible.")
+            else:
+                print(colorama.Fore.YELLOW + f"API URL {url} is not accessible or requires authorization.")
+
+        return accessible_urls
+
+async def main():
+    """Main asynchronous function to run the program."""
+    clear_screen()
+    colorama.init(autoreset=True)
+    print(colorama.Fore.RED + """
+▓██   ██▓ ▒█████   █    ██   ██████ ▓█████ ▓█████  ███▄ ▄███▓▓█████ 
+ ▒██  ██▒▒██▒  ██▒ ██  ▓██▒▒██    ▒ ▓█   ▀ ▓█   ▀ ▓██▒▀█▀ ██▒▓█   ▀ 
+  ▒██ ██░▒██░  ██▒▓██  ▒██░░ ▓██▄   ▒███   ▒███   ▓██    ▓██░▒███   
+  ░ ▐██▓░▒██   ██░▓▓█  ░██░  ▒   ██▒▒▓█  ▄ ▒▓█  ▄ ▒██    ▒██ ▒▓█  ▄ 
+  ░ ██▒▓░░ ████▓▒░▒▒█████▓ ▒██████▒▒░▒████▒░▒████▒▒██▒   ░██▒░▒████▒
+   ██▒▒▒ ░ ▒░▒░▒░ ░▒▓▒ ▒ ▒ ▒ ▒▓▒ ▒ ░░░ ▒░ ░░░ ▒░ ░░ ▒░   ░  ░░░ ▒░ ░
+ ▓██ ░▒░   ░ ▒ ▒░ ░░▒░ ░ ░ ░ ░▒  ░ ░ ░ ░  ░ ░ ░  ░░  ░      ░ ░ ░  ░
+ ▒ ▒ ░░  ░ ░ ░ ▒   ░░░ ░ ░ ░  ░  ░     ░      ░   ░      ░      ░   
+ ░ ░         ░ ░     ░           ░     ░  ░   ░  ░       ░      ░  ░
+ ░ ░                                                                
+                                                             
+                    Python IP Tracker - K11E3R
+    """)
+
+    # Test API URLs for accessibility
+    accessible_urls = await test_api_urls()
+    if not accessible_urls:
+        print(colorama.Fore.RED + "No accessible API URLs found. Exiting.")
+        return
+
+    async with aiohttp.ClientSession() as session:
+        while True:
+            ip = input("Enter target IP address (or 'exit' to quit): ").strip()
+            if ip.lower() == 'exit':
+                break
+
+            if not ip:
+                print(colorama.Fore.RED + "Please enter a valid IP address.")
+                continue
+
+            # Fetch IP data from accessible URLs
+            ip_data = None
+            for url in accessible_urls:
+                ip_data = await fetch_ip_data(session, url, ip)
+                if ip_data:
+                    break
+
+            print_ip_data(ip_data)
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print(colorama.Fore.RED + "\nProgram terminated by user.")
